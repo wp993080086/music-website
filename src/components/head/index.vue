ad6d7b0eebf10da73a695fe70c94738a6bf0f899
pdd11997110103 <993080086@qq.com>
2021-05-12 22:05:29 +0800

    全局换肤demo；

--- src/components/head/index.vue
+++ src/components/head/index.vue
@@ -38,7 +38,7 @@ export default {
 </script>
 
 <style lang='less' scoped>
-@import '../../assets/css/changeColor.less';
+@import '../../assets/css/globalColor.less';
 	#head{
 		width: 100%;
 		.box{
