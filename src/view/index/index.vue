245dc2a7ee7dc243937d2c021928ac7eb32a0514
@wangpeng <993080086@qq.com>
2021-05-14 17:08:23 +0800

    全屏切换；

--- src/view/index/index.vue
+++ src/view/index/index.vue
@@ -15,7 +15,8 @@ export default {
 	},
 	data() {
 		return {
-			title: 'hello world'
+			title: 'hello world',
+			fullscreen: false
 		}
 	},
 	created() {
@@ -27,6 +28,33 @@ export default {
 	methods: {
 		console() {
 			console.log(this.$baseUrl)
+		},
+		change_full_screen() {
+			// 全屏切换函数
+			const element = document.documentElement
+			if (this.fullscreen) {
+				if (document.exitFullscreen) {
+					document.exitFullscreen()
+				} else if (document.webkitCancelFullScreen) {
+					document.webkitCancelFullScreen()
+				} else if (document.mozCancelFullScreen) {
+					document.mozCancelFullScreen()
+				} else if (document.msExitFullscreen) {
+					document.msExitFullscreen()
+				}
+			} else {
+				if (element.requestFullscreen) {
+					element.requestFullscreen()
+				} else if (element.webkitRequestFullScreen) {
+					element.webkitRequestFullScreen()
+				} else if (element.mozRequestFullScreen) {
+					element.mozRequestFullScreen()
+				} else if (element.msRequestFullscreen) {
+					// IE11
+					element.msRequestFullscreen()
+				}
+			}
+			this.fullscreen = !this.fullscreen // 判断全屏状态
 		}
 	}
 }
